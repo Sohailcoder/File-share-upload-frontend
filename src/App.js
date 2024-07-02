3589af946a66a8b78a43ac679ecaa0b3d7917ba5
Sohailcode <100mohdsohail@gmail.com>
2024-07-02 14:18:47 +0530

    app.js changes done

--- src/App.js
+++ src/App.js
@@ -2,15 +2,11 @@ import { useState, useEffect, useRef } from 'react';
 import './App.css';
 import { uploadFile } from './service/api';
 
-
 function App() {
-  const [file, setFile] = useState('');
+  const [file, setFile] = useState(null);
   const [result, setResult] = useState('');
-  
 
   const fileInputRef = useRef();
-
-  const url = 'https://i.pinimg.com/originals/16/46/24/1646243661201a0892cc4b1a64fcbacf.jpg';
 
   useEffect(() => {
     const getImage = async () => {
@@ -19,37 +15,41 @@ function App() {
         data.append("name", file.name);
         data.append("file", file);
 
-        const response = await uploadFile(data);
-        setResult(response.path);
+        try {
+          const response = await uploadFile(data);
+          setResult(response.path);
+        } catch (error) {
+          console.error("Error uploading file:", error);
+        }
       }
-    }
+    };
     getImage();
-  }, [file])
+  }, [file]);
 
   const onUploadClick = () => {
     fileInputRef.current.click();
-  }
+  };
+
   return (
     <div className="App">
       <div className='container'>
-      <img src={url} className='img'  alt="Image"/>
-      <div className='wrapper'>
-        <h1>Simple file sharing!</h1>
-        <p>Upload and share the download link.</p>
-        
-        <button onClick={() => onUploadClick()}>Upload</button>
-        <input
-          type="file"
-          ref={fileInputRef}
-          style={{ display: "none" }}
-          onChange={(e) => setFile(e.target.files[0])}
-        />
-        <a href={result} target='_blank' rel="noopener">{result}</a> 
+        <img src="https://i.pinimg.com/originals/16/46/24/1646243661201a0892cc4b1a64fcbacf.jpg" className='img' alt="A beautiful landscape" />
+        <div className='wrapper'>
+          <h1>Simple file sharing!</h1>
+          <p>Upload and share the download link.</p>
+          
+          <button onClick={onUploadClick}>Upload</button>
+          <input
+            type="file"
+            ref={fileInputRef}
+            style={{ display: "none" }}
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+          {result && <a href={result} target='_blank' rel="noopener noreferrer">{result}</a>}
+        </div>
       </div>
-    </div>
     </div>
   );
 }
-
 
 export default App;
