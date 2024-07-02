a8b5aed92819c2807aa4b6dd82b9fa630fb1beda
Sohailcode <100mohdsohail@gmail.com>
2024-07-02 13:59:10 +0530

    changes done in image attribute

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
   return (
     <div className="App">
       <div className='container'>
-      <img src={url} className='img' />
+      <img src={url} className='img'  alt="Image"/>
       <div className='wrapper'>
         <h1>Simple file sharing!</h1>
         <p>Upload and share the download link.</p>
@@ -45,7 +45,7 @@ function App() {
           onChange={(e) => setFile(e.target.files[0])}
         />
 
-        <a href={result} target='_blank'>{result}</a> 
+        <a href={result} target='_blank' rel="noopener">{result}</a> 
       </div>
     </div>
     </div>
